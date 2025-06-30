9e3cb98ea550a5fa62cf0841db92724de4444b97
RajeshMajumder97 <rajeshnbp9051@gmail.com>
2025-06-30 23:27:02 +0530

    tt

--- pages/Normal Mean Estimation.py
+++ pages/Normal Mean Estimation.py
@@ -7,16 +7,18 @@ from scipy.special import erf
 st.set_page_config(page_title="Normal Mean Estimation",
                    page_icon="🧊")
 
-#MainMenu {visibility: hidden;}
-hide_st_style = """
-<style>
-MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-header {visibility: hidden;}
-</style>
-"""
-st.markdown(hide_st_style, unsafe_allow_html=True)
-
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 st.title("Sample Size Calculation for Mean: Mean Estimation")
 ## Functuion
