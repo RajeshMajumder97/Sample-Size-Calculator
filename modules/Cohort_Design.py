992a0896b0e8f09420b2e3b04d43a746d663994c
RajeshMajumder97 <rajeshnbp9051@gmail.com>
2025-07-08 15:43:18 +0530

    edf

--- modules/Cohort_Design.py
+++ modules/Cohort_Design.py
@@ -6,7 +6,7 @@ from scipy.special import erf
 import math
 def main():
 
-    # Streamlit App
+    st.title("Sample Size Calculation for Case Control Design: Odds Ratio | H0: OR=1")
     st.markdown(
         """
         <style>
@@ -17,7 +17,6 @@ def main():
         """,
         unsafe_allow_html=True
     )
-    st.title("Sample Size Calculation for Cohort Design: Relative Risk | H0: RR=1")
     ## Functuion
     def nSampleRR(p1=0.5,RR=1.0,Pw=0.8,Conf=0.95,designEf=1,dropOut=0):
         p2= p1*RR
