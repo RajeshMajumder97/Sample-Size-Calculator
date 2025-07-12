024774278574942213b6c24363ae262e80be9ce7
RajeshMajumder97 <rajeshnbp9051@gmail.com>
2025-07-12 16:26:31 +0530

    Updated

--- Home.py
+++ Home.py
@@ -229,25 +229,5 @@ else:
 
 
 
-import streamlit.components.v1 as components
-
-# ✅ Tawk.to Chatbot Embed (Replace with your real widget ID)
-components.html("""
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/6871795c3606072bf849ed1e/1ivtk457m';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
-""", height=0)
-
-
 
 
